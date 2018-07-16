e435864196a6cdc182add80dfe01e395dbfc969e
Jihye Lee <psycho5cafe@gmail.com>
2018-07-16 17:47:48 +0900

    Update ad-data-analysis-with-r.R

--- ad-data-analysis-with-r.R
+++ ad-data-analysis-with-r.R
@@ -69,20 +69,12 @@ result <- bq_table_download(tb,
                             max_results = 1000000)
 
 head(result)
-print(result)
+dt<- result 
 
 #### Data wrangling #### 
-#ref https://rpubs.com/jmhome/R_data_processing
-data <- result
-dt <- data # data backup
-
 # Exporting the data from directory 
-getwd()
-dt <- read.csv("ga360-data-20180621-2.csv")
-
 library(readr)
-#dt <- read_csv("ga360-data-20180621-2.csv")
-dt <- read_csv("ga360-data-may-20180704.csv")
+dt <- read_csv("ga360-data-may-20180704.csv") #read_csv('data.csv')
 str(data)
 head(data)
 
@@ -92,189 +84,162 @@ dt <- na.omit(data)
 
 # 데이터 기기 타입에 따라 나누기 
 #기기타입 변환 
+
 #MOBILE 
 dt$isMobile <-
   ifelse(dt$deviceCategory %in% 'mobile', 1,
   ifelse(dt$deviceCategory %in% 'tablet', 2,
   	0))
-
 #PC 
 dt$isPC <- ifelse(dt$deviceCategory %in% 'desktop', 1, 0)
 
 #pc/mobile 변환 
-dtPC <- subset(dt, isPC == 1)
-dtMobile <- subset(dt, isPC == 0)
+dtMobile <- subset(dt, isPC == 0) #모바일, 태블릿 사용자만 남음 
 dt <- dtMobile #데이터 복사 
 dt <- dt[, c(3:14)]
 
 # 데이터 샘플링  
-# 샘플링 방법론 참고: http://rfriend.tistory.com/58 
-dt <- sample(dt, 1000, replace=F)
-
-#— 2. 그룹수, 70% : 30%
-idx <- sample(2, nrow(dt), replace = TRUE, prob = c(0.7, 0.3))
-
-idx <- sample(2, nrow(data), replace = TRUE, prob = c(0.7, 0.3))
-train <- data[idx == 1, ]
-test <- data[idx == 2, ]
-
-# 불균형을 줄이는 샘플링 
-dtmobile$isRegister2 <- dtmobile$isRegister
-
 #https://thebook.io/006723/ch10/06/01/ 
 library(caret) 
 train <- upSample(subset(dt, select = -isRegister2), dt$isRegister) #업샘플링: 해당 분류에 속하는 데이터가 적은 쪽을 표본으로 더 많이 추출하는 방법
 train2 <- downSample(subset(dt, select = -isRegister2), dt$isRegister) #다운샘플링: 데이터가 많은 쪽을 적게 추출하는 방법
 　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　 #다운샘플링 하면 5:5로 비율 맞춰져서 데이터 축소 됨 
 # downsample한 데이터의 상황 점검 
-length(which(train$isRegister==0))
-length(which(train$isRegister==1))
-
 length(which(train2$isRegister==0))
 length(which(train2$isRegister==1))
 
-hist(train2$isRegister)
+#해당 날짜에 데이터 존재 유무 확인 
+length(which(train2$TABLE_DATE=="20180531")) #
 
-#sampling http://mgdjaxo.blogspot.com/2015/06/r-sample-function.html
+#operating system 확인 
+length(which(train2$operatingSystem =="Blackberry"))
+
+#브라우저 데이터 분포 파악 
+length(which(train2$browser=="Android Webview")) #2665
 
 # data distribution (EDA)
-hist(dt$isRegister)
-hist(dt$isApply)
+hist(train2$isRegister)
+hist(train2$ivsApply)
 
-summary(dt) #데이터 변수별 값 현황 요약 
-var(dt) #분산
-sd(dt) #표준편차 
-
+summary(train2) #데이터 변수별 값 현황 요약 
+var(train2) #분산
+sd(train2) #표준편차 
 
 # ----- 데이터 타입 바꾸기  ----- # 
 
-# factor to numeric 
-dt$broswerVersion <- as.numeric(dt$broswerVersion)
-dt$operatingSystem <- as.factor(dt$operatingSystem) #factor로 바꾸면 summary(data)에서 데이터 분포를 파악할 때 각 변수 내에 분포하는 카테고리 종류가 몇개씩 들어있는지 알 수 있음 
+# factor<->numeric 
+#train2$broswerVersion <- as.numeric(train2$broswerVersion)
+#train2$operatingSystem <- as.factor(train2$operatingSystem) #factor로 바꾸면 summary(data)에서 데이터 분포를 파악할 때 각 변수 내에 분포하는 카테고리 종류가 몇개씩 들어있는지 알 수 있음 
 
-dt$deviceCategory <- as.factor(dt$deviceCategory)
-dt$mobileDeviceBranding <- as.factor(dt$mobileDeviceBranding)
-dt$mobileDeviceModel <- as.factor(dt$mobileDeviceModel)
+#train2$deviceCategory <- as.factor(train2$deviceCategory)
+#train2$mobileDeviceBranding <- as.factor(train2$mobileDeviceBranding)
+#train2$mobileDeviceModel <- as.factor(train2$mobileDeviceModel)
 
-dt$deviceCategory2 <- as.factor(dt$deviceCategory2)
-dt$mobileDeviceBranding2 <- as.factor(dt$mobileDeviceBranding2)
-dt$operatingSystem2 <- as.factor(dt$operatingSystem2)
-dt$deviceCategory2 <- as.factor(dt$deviceCategory2)
+#train2$deviceCategory2 <- as.factor(train2$deviceCategory2)
+#train2$mobileDeviceBranding2 <- as.factor(train2$mobileDeviceBranding2)
+#train2$operatingSystem2 <- as.factor(train2$operatingSystem2)
+#train2$deviceCategory2 <- as.factor(train2$deviceCategory2)
 
-dt$isMobile <- as.factor(dt$isMobile)
-dt$isPC <- as.factor(dt$isPC)
+#train2$isMobile <- as.factor(train2$isMobile)
+#train2$isPC <- as.factor(train2$isPC)
 
 #종속변수 
-dt$isRegister <- as.factor(dt$isRegister)
-dt$isApply <- as.factor(dt$isApply)
+#train2$isRegister <- as.factor(train2$isRegister)
+#train2$isApply <- as.factor(train2$isApply)
 
 
 # 카테고리 데이터의 분포 알아보기 (data_type = factor일 때만 사용 가능)
-levels(dt$mobileDeviceBranding)
-levels(dt$browser)
-
-# 접수와 승인의 갯수 알아보기 
-length(which(dt$isRegister==1))  #isregister에서 1의 갯수 (접수)
-length(which(dt$isApply==1))  #isapply에서 1의 갯수 (승인)
-
-#해당 날짜에 데이터 존재 유무 확인 
-length(which(dt$TABLE_DATE=="20180531")) #
-
-#operating system 확인 
-length(which(dt$operatingSystem =="Blackberry"))
-
-#브라우저 데이터 분포 파악 
-length(which(dt$browser=="Android Webview")) #2665
-
+levels(train2$mobileDeviceBranding)
+levels(train2$browser)
 
 #카테고리 데이터 레이블링하기 (Sample code)
 
 # 모바일 OS 시스템 
-dt$operatingSystem2 <- 
-  ifelse(dt$operatingSystem %in% 'Android', 1,
-  ifelse(dt$operatingSystem %in% 'iOS', 2, 
-  ifelse(dt$operatingSystem %in% 'Blackberry', 3, 
-  ifelse(dt$operatingSystem %in% 'Chrome OS', 4,
-  ifelse(dt$operatingSystem %in% 'Linux', 5,
-  ifelse(dt$operatingSystem %in% 'Macintosh', 6,
-  ifelse(dt$operatingSystem %in% 'Windows', 7,
+train2$operatingSystem2 <- 
+  ifelse(train2$operatingSystem %in% 'Android', 1,
+  ifelse(train2$operatingSystem %in% 'iOS', 2, 
+  ifelse(train2$operatingSystem %in% 'Blackberry', 3, 
+  ifelse(train2$operatingSystem %in% 'Chrome OS', 4,
+  ifelse(train2$operatingSystem %in% 'Linux', 5,
+  ifelse(train2$operatingSystem %in% 'Macintosh', 6,
+  ifelse(train2$operatingSystem %in% 'Windows', 7,
   	0)))))))
 
 # 모바일 기기 브랜드 
-dt$mobileDeviceBranding2 <-
-  ifelse(dt$mobileDeviceBranding %in% 'Samsung', 1,
-  ifelse(dt$mobileDeviceBranding %in% 'Apple', 2, 
-  ifelse(dt$mobileDeviceBranding %in% 'LG', 3, 
-  ifelse(dt$mobileDeviceBranding %in% 'Google', 4,
-  ifelse(dt$mobileDeviceBranding %in% 'Huawei', 5,
-  ifelse(dt$mobileDeviceBranding %in% 'KT Tech', 6,
-  ifelse(dt$mobileDeviceBranding %in% 'Lava', 7,
-  ifelse(dt$mobileDeviceBranding %in% 'Pantech', 8,
-  ifelse(dt$mobileDeviceBranding %in% 'Sony', 9,  
-  ifelse(dt$mobileDeviceBranding %in% 'Xiaomi', 10,
-  ifelse(dt$mobileDeviceBranding %in% 'Windows', 11,
-  ifelse(dt$mobileDeviceBranding %in% 'Blackberry', 16, # 16 = 기타 브랜드 혹은 원본 데이터에서 분포가 낮은 브랜드들 
-  ifelse(dt$mobileDeviceBranding %in% 'Microsoft', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Alldaymall', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Asus', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'Amazon', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'Alcatel', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'Nokia', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'Acer', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'Canaima', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'Meizu', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'TCL', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'OPPO', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'IPPO', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'OnePlus', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Luna', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Wiko', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Vivo', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'TG & Company', 16, 
-  ifelse(dt$mobileDeviceBranding %in% 'CAT', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Point Mobile', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'LeEco', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Sharp', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Sky Devices', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'SonyEricsson', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'HTC', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Motorola', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Nvidia', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Lava', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Nextbit', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Hannspree', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Chuwi', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Lenovo', 16,
-  ifelse(dt$mobileDeviceBranding %in% 'Opera Software', 16,
+train2$mobileDeviceBranding2 <-
+  ifelse(train2$mobileDeviceBranding %in% 'Samsung', 1,
+  ifelse(train2$mobileDeviceBranding %in% 'Apple', 2, 
+  ifelse(train2$mobileDeviceBranding %in% 'LG', 3, 
+  ifelse(train2$mobileDeviceBranding %in% 'Google', 4,
+  ifelse(train2$mobileDeviceBranding %in% 'Huawei', 5,
+  ifelse(train2$mobileDeviceBranding %in% 'KT Tech', 6,
+  ifelse(train2$mobileDeviceBranding %in% 'Lava', 7,
+  ifelse(train2$mobileDeviceBranding %in% 'Pantech', 8,
+  ifelse(train2$mobileDeviceBranding %in% 'Sony', 9,  
+  ifelse(train2$mobileDeviceBranding %in% 'Xiaomi', 10,
+  ifelse(train2$mobileDeviceBranding %in% 'Windows', 11,
+  ifelse(train2$mobileDeviceBranding %in% 'Blackberry', 16, # 16 = 기타 브랜드 혹은 원본 데이터에서 분포가 낮은 브랜드들 
+  ifelse(train2$mobileDeviceBranding %in% 'Microsoft', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Alldaymall', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Asus', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'Amazon', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'Alcatel', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'Nokia', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'Acer', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'Canaima', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'Meizu', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'TCL', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'OPPO', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'IPPO', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'OnePlus', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Luna', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Wiko', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Vivo', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'TG & Company', 16, 
+  ifelse(train2$mobileDeviceBranding %in% 'CAT', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Point Mobile', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'LeEco', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Sharp', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Sky Devices', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'SonyEricsson', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'HTC', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Motorola', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Nvidia', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Lava', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Nextbit', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Hannspree', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Chuwi', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Lenovo', 16,
+  ifelse(train2$mobileDeviceBranding %in% 'Opera Software', 16,
   	0)))))))))))))))))))))))))))))))))))))))))))))    
 
 # 모바일 브라우저 
-dt$browser2 <-
-  ifelse(dt$browser %in% "YaBrowser", 1,  #1 = 기타 etc
-  ifelse(dt$browser %in% "Amazon Silk", 1,  
-  ifelse(dt$browser %in% "Coc Coc", 1, 
-  ifelse(dt$browser %in% "Edge", 1,  
-  ifelse(dt$browser %in% "BrowserNG", 1,
-  ifelse(dt$browser %in% "IE with Chrome frame", 1, 
-  ifelse(dt$browser %in% "NokiaC7-00", 1, 
-  ifelse(dt$browser %in% "Puffin", 1, 
-  ifelse(dt$browser %in% "Sarari (in-app)", 1, 
-  ifelse(dt$browser %in% "UC Browser", 1, 
-  ifelse(dt$browser %in% "YaBrowser", 1,
-  ifelse(dt$browser %in% "Java", 1, 
-  ifelse(dt$browser %in% "Chrome", 2, 
-  ifelse(dt$browser %in% "Samsung Internet", 3,
-  ifelse(dt$browser %in% "Android Webview", 4, 
-  ifelse(dt$browser %in% "Safari", 5, 
-  ifelse(dt$browser %in% "Safari(in-app)", 5, 
-  ifelse(dt$browser %in% "Internet Explorer", 6, 
-  ifelse(dt$browser %in% "Opera", 7,  
-  ifelse(dt$browser %in% "Firefox", 8, 
-  ifelse(dt$browser %in% "BlackBerry", 9, 
-  ifelse(dt$browser %in% "Mozilla Compatible Agent", 10,
-  ifelse(dt$browser %in% "Android Runtime", 11, #11 = android 계열의 브라우저들  
-  ifelse(dt$browser %in% "Android WebApps", 11,  
-  ifelse(dt$browser %in% "Android Browser", 11,  
+train2$browser2 <-
+  ifelse(train2$browser %in% "YaBrowser", 1,  #1 = 기타 etc
+  ifelse(train2$browser %in% "Amazon Silk", 1,  
+  ifelse(train2$browser %in% "Coc Coc", 1, 
+  ifelse(train2$browser %in% "Edge", 1,  
+  ifelse(train2$browser %in% "BrowserNG", 1,
+  ifelse(train2$browser %in% "IE with Chrome frame", 1, 
+  ifelse(train2$browser %in% "NokiaC7-00", 1, 
+  ifelse(train2$browser %in% "Puffin", 1, 
+  ifelse(train2$browser %in% "Sarari (in-app)", 1, 
+  ifelse(train2$browser %in% "UC Browser", 1, 
+  ifelse(train2$browser %in% "YaBrowser", 1,
+  ifelse(train2$browser %in% "Java", 1, 
+  ifelse(train2$browser %in% "Chrome", 2, 
+  ifelse(train2$browser %in% "Samsung Internet", 3,
+  ifelse(train2$browser %in% "Android Webview", 4, 
+  ifelse(train2$browser %in% "Safari", 5, 
+  ifelse(train2$browser %in% "Safari(in-app)", 5, 
+  ifelse(train2$browser %in% "Internet Explorer", 6, 
+  ifelse(train2$browser %in% "Opera", 7,  
+  ifelse(train2$browser %in% "Firefox", 8, 
+  ifelse(train2$browser %in% "BlackBerry", 9, 
+  ifelse(train2$browser %in% "Mozilla Compatible Agent", 10,
+  ifelse(train2$browser %in% "Android Runtime", 11, #11 = android 계열의 브라우저들  
+  ifelse(train2$browser %in% "Android WebApps", 11,  
+  ifelse(train2$browser %in% "Android Browser", 11,  
          0)))))))))))))))))))))))))
 
 # device model
@@ -296,9 +261,8 @@ train2$mobilemodel <-
 # ----------------------------------------------------------------------- # 
 
 # 특정 열 삭제 혹은 추출 
-
-dt2 <-dt[, c(1:5)] #열 
-dt2 <- dt[c(3:10), ] #행
+dt <-dt[, c(1:5)] #열 
+dt <- dt[c(3:10), ] #행
 test <- train2[, c(2, 4, 6, 7, 10, 13:17)]
 
 # ----------------------------------------------------------------------- # 
@@ -327,61 +291,48 @@ chisq.test(table4)
 
 # 데이터 카테고리 범주 재설정 
 #모바일 기종
-dt$mobilemodel <- 
-  ifelse(dt$mobilemodel %in% 1, 1,
-  ifelse(dt$mobilemodel %in% 2, 2, 
-  ifelse(dt$mobilemodel %in% 3, 3, #갤럭시 a, j 결합 (중급+보급형)
-  ifelse(dt$mobilemodel %in% 4, 3,
-  ifelse(dt$mobilemodel %in% 4, 4,
-  ifelse(dt$mobilemodel %in% 5, 5,
-  ifelse(dt$mobilemodel %in% 7, 9, # 7, 8 기타 기종으로 결합 
-  ifelse(dt$mobilemodel %in% 8, 9,
+train2$mobilemodel <- 
+  ifelse(train2$mobilemodel %in% 1, 1,
+  ifelse(train2$mobilemodel %in% 2, 2, 
+  ifelse(train2$mobilemodel %in% 3, 3, #갤럭시 a, j 결합 (중급+보급형)
+  ifelse(train2$mobilemodel %in% 4, 3,
+  ifelse(train2$mobilemodel %in% 4, 4,
+  ifelse(train2$mobilemodel %in% 5, 5,
+  ifelse(train2$mobilemodel %in% 7, 9, # 7, 8 기타 기종으로 결합 
+  ifelse(train2$mobilemodel %in% 8, 9,
  0))))))))
 
 #브라우저 종류 
-dt$browser2 <- 
-  ifelse(dt$browser2 %in% 1, 0,
-  ifelse(dt$browser2 %in% 2, 2, #chrome  
-  ifelse(dt$browser2 %in% 3, 3, #samsung 
-  ifelse(dt$browser2 %in% 4, 4, 
-  ifelse(dt$browser2 %in% 5, 5,
-  ifelse(dt$browser2 %in% 6, 6,
+train2$browser2 <- 
+  ifelse(train2$browser2 %in% 1, 0,
+  ifelse(train2$browser2 %in% 2, 2, #chrome  
+  ifelse(train2$browser2 %in% 3, 3, #samsung 
+  ifelse(train2$browser2 %in% 4, 4, 
+  ifelse(train2$browser2 %in% 5, 5,
+  ifelse(train2$browser2 %in% 6, 6,
  0)))))) # 블랙베리, 오페라 결합 없이 0으로 보내버림 
 
-
 #모바일 기기 브랜드  
-dt$mobileDeviceBranding2 <- 
-  ifelse(dt$mobileDeviceBranding2 %in% 1, 1, #삼성
-  ifelse(dt$mobileDeviceBranding2 %in% 2, 2, 
-  ifelse(dt$mobileDeviceBranding2 %in% 3, 3, 
-  ifelse(dt$mobileDeviceBranding2 %in% 4, 4,
-  ifelse(dt$mobileDeviceBranding2 %in% 5, 5,
+train2$mobileDeviceBranding2 <- 
+  ifelse(train2$mobileDeviceBranding2 %in% 1, 1, #삼성
+  ifelse(train2$mobileDeviceBranding2 %in% 2, 2, 
+  ifelse(train2$mobileDeviceBranding2 %in% 3, 3, 
+  ifelse(train2$mobileDeviceBranding2 %in% 4, 4,
+  ifelse(train2$mobileDeviceBranding2 %in% 5, 5,
    0)))))
-    
-    
 
-# ----------------------------------------------------------------------- # 
+#독립성검정(재실시)
+table1 <- xtabs(~operatingSystem2+isRegister, data=train2) 
+table2 <- xtabs(~mobileDeviceBranding2+isRegister, data=train2) 
+table3 <- xtabs(~browser2+isRegister, data=train2) 
+table4 <- xtabs(~mobilemodel+isRegister, data=train2) 
 
-#카이제곱검정 
-# 설명 참고: http://rfriend.tistory.com/112
-
-library(gmodels) 
-library(vcd) 
-
-attach(train2) 
-#detach(train2)
-
-CrossTable(deviceCategory, isRegister, # crosstable = 교차분석
-            expected = TRUE, # expected frequency
-            chisq = TRUE) # chisq-test 
-
-os <- table(df$operatingSystem, df$isRegister)
-model <- table(dt.na$mobileDeviceModel, dt.na$isRegister)
-
-chisq.test(model)
-
-
-# ----------------------------------------------------------------------- # 
-
-# csv exporting 
-write.csv(data, file="ga360-data-20180704-2.csv", row.names = TRUE) 
+table1
+table2
+table3
+table4
+ 
+chisq.test(table1)
+chisq.test(table2)
+chisq.test(table3)
+chisq.test(table4)
